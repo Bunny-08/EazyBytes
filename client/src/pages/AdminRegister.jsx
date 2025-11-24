5c06bba77c8a2f72e128257ce701b1c2f88b77b6
Shaik Badrujama <140937316+Bunny-08@users.noreply.github.com>
2025-11-24 18:04:08 +0530

    EazyBytes

--- client/src/pages/AdminRegister.jsx
+++ client/src/pages/AdminRegister.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "../styles/global.css";
-import "./adminlogin.css";
+import "./AdminLogin.css";
 import { useNavigate } from "react-router-dom";
 
 export default function AdminRegister() {
